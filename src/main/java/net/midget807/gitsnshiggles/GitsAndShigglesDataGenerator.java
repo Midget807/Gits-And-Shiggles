36477f65a12e2adac2394a4ccdb74adfcd5d9a18
Midget807 <phu7mai@gmail.com>
2025-08-01 18:19:18 +0930

    Initial commit

---
+++ src/main/java/net/midget807/gitsnshiggles/GitsAndShigglesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.midget807.gitsnshiggles;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GitsAndShigglesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
